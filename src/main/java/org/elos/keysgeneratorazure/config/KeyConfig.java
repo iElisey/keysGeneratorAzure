4be11ff2d12acd8f300d2bae517ccac4cd524730
iElisey <yelisei.osadchyi@gmail.com>
2024-09-03 22:58:43 +0300

    Changing keys

--- src/main/java/org/elos/keysgeneratorazure/config/KeyConfig.java
+++ src/main/java/org/elos/keysgeneratorazure/config/KeyConfig.java
@@ -16,8 +16,6 @@ public class KeyConfig {
         KEY_CONFIGS.put("TWERK", new KeyDetails("61308365-9d16-4040-8bb0-2f4a4c69074c", "61308365-9d16-4040-8bb0-2f4a4c69074c", 20000));
         KEY_CONFIGS.put("POLYSPHERE", new KeyDetails("2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", 3000));
         KEY_CONFIGS.put("TRIM", new KeyDetails("ef319a80-949a-492e-8ee0-424fb5fc20a6", "ef319a80-949a-492e-8ee0-424fb5fc20a6", 20000));
-        KEY_CONFIGS.put("CAFE", new KeyDetails("bc0971b8-04df-4e72-8a3e-ec4dc663cd11", "bc0971b8-04df-4e72-8a3e-ec4dc663cd11", 22000));
-        KEY_CONFIGS.put("GANGS", new KeyDetails("b6de60a0-e030-48bb-a551-548372493523", "c7821fa7-6632-482c-9635-2bd5798585f9", 40000));
         KEY_CONFIGS.put("ZOO", new KeyDetails("b2436c89-e0aa-4aed-8046-9b0515e1c46b", "b2436c89-e0aa-4aed-8046-9b0515e1c46b", 22000));
     }
 
