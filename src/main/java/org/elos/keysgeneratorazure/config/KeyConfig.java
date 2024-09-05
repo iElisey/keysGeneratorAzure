130bfba5bf6db3d2ab0ad974c82d47ac4e996d91
iElisey <yelisei.osadchyi@gmail.com>
2024-09-05 18:23:11 +0300

    Added new key prefix STONE

--- src/main/java/org/elos/keysgeneratorazure/config/KeyConfig.java
+++ src/main/java/org/elos/keysgeneratorazure/config/KeyConfig.java
@@ -17,8 +17,9 @@ public class KeyConfig {
 //        KEY_CONFIGS.put("POLYSPHERE", new KeyDetails("2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", 3000));
 //        KEY_CONFIGS.put("TRIM", new KeyDetails("ef319a80-949a-492e-8ee0-424fb5fc20a6", "ef319a80-949a-492e-8ee0-424fb5fc20a6", 20000));
 //        KEY_CONFIGS.put("ZOO", new KeyDetails("b2436c89-e0aa-4aed-8046-9b0515e1c46b", "b2436c89-e0aa-4aed-8046-9b0515e1c46b", 22000));
-        KEY_CONFIGS.put("TILE", new KeyDetails("e68b39d2-4880-4a31-b3aa-0393e7df10c7", "e68b39d2-4880-4a31-b3aa-0393e7df10c7", 20000));
-        KEY_CONFIGS.put("FLUFF", new KeyDetails("112887b0-a8af-4eb2-ac63-d82df78283d9", "112887b0-a8af-4eb2-ac63-d82df78283d9", 120000));
+//        KEY_CONFIGS.put("TILE", new KeyDetails("e68b39d2-4880-4a31-b3aa-0393e7df10c7", "e68b39d2-4880-4a31-b3aa-0393e7df10c7", 20000));
+//        KEY_CONFIGS.put("FLUFF", new KeyDetails("112887b0-a8af-4eb2-ac63-d82df78283d9", "112887b0-a8af-4eb2-ac63-d82df78283d9", 120000));
+        KEY_CONFIGS.put("STONE", new KeyDetails("04ebd6de-69b7-43d1-9c4b-04a6ca3305af", "04ebd6de-69b7-43d1-9c4b-04a6ca3305af", 20000));
     }
 
     // Function to get appToken and promoId based on the key
